05264df90c21cc7536556b3dc56463105b092ee0
unknown <sakethphaneendra@gmail.com>
2024-01-14 22:08:53 +0530

    Complted the Array Basics

---
+++ Arrays/src/SearchingAlgos/AscendingSort.java
@@ -0,0 +1,11 @@
+package SearchingAlgos;
+import java.util.Arrays;
+public class AscendingSort {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hi");
+
+	}
+
+}
---
+++ Arrays/src/SortingandSearching/see.java
@@ -0,0 +1,10 @@
+package SortingandSearching;
+
+public class see {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hi");
+	}
+
+}
